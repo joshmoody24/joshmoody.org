377df2cf4bb6633c608156954a9e66bc4fa494ee
Josh Moody <joshmoody24@gmail.com>
2023-09-01 01:02:00 -0600

    finished blazor blog post

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'joshmoody.org';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'The hub for everything made by Josh Moody';
